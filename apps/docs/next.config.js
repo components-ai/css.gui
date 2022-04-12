fcc573bb19898d115c5ff29f79d0a5abc5f9ea05
John Otander <johnotander@gmail.com>
2022-04-12 10:46:18 -0600

    Add comment documenting attempted fix

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -3,6 +3,12 @@ const withTM = require('next-transpile-modules')(['gui'])
 
 module.exports = withTM({
   reactStrictMode: true,
+  // This is an attempted fix for a bug in the Turbo's usage of
+  // next-transpile-modules, though as configured it doesn't
+  // seem to work and I'm not sure why. Will kick the can for now.
+  //
+  // • https://github.com/vercel/turborepo/issues/338
+  // • https://github.com/martpie/next-transpile-modules
   webpack: (config, options) => {
     if (options.isServer) {
       config.externals = ['react', ...config.externals]
