9302e8aab7da66827c0dd84e66f951da6f6b6517
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2022-04-25 15:17:13 -0700

    remove console.log

--- packages/gui/src/lib/elements.ts
+++ packages/gui/src/lib/elements.ts
@@ -2,6 +2,5 @@ import { elements } from '../data/elements'
 
 export const isElement = (str: string): boolean => {
   const els = elements.filter((el) => str === el)
-  console.log(els, str)
   return !!elements.filter((el) => str === el).length
 }
