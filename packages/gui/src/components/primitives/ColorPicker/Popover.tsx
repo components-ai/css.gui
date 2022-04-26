7572efb84098ff1213850665f7bbea695af99138
Dean Blacc <deanblacc@googlemail.com>
2022-04-26 13:49:55 +0100

    small clean up

--- packages/gui/src/components/primitives/ColorPicker/Popover.tsx
+++ packages/gui/src/components/primitives/ColorPicker/Popover.tsx
@@ -76,11 +76,10 @@ function Swatch({ value }: { value: string }) {
       {/* The checkerboard causes weird aliasing to occur on Firefox,
        * so we only show it when there is a transparency */}
       {isTransparent && (
-        <Checkerboard sx={{ position: 'absolute', inset: 0 }} />
+        <Checkerboard sx={{ inset: 0 }} />
       )}
       <div
         sx={{
-          // position: 'absolute',
           inset: 0,
           ...withFallback(value, (color) => ({ backgroundColor: color })),
         }}
