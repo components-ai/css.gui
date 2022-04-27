d16f1f05ee32c8f1f21b119418722dd63f44390d
Dean Blacc <deanblacc@googlemail.com>
2022-04-27 15:57:07 +0100

    piece together font tag data

--- packages/gui/src/components/RenderElement.tsx
+++ packages/gui/src/components/RenderElement.tsx
@@ -26,8 +26,78 @@ export const RenderElement = ({
   )
 }
 
+export const plusify = (name: string) => name.replace(/\s/g, '+')
+
+type FontFamily = {
+  name: string
+  weights: (string | number)[]
+  styles: string[]
+}
+export const toGoogleFontUrl = async (families: FontFamily[]) => {
+  if (!families?.length) return null
+
+  let familiesFmt: any[] = []
+
+  families.forEach((family) => {
+    let name = plusify(family.name)
+    family.weights.forEach((weight) => {
+      family.styles.forEach((style) => {
+        let query = `family=${name}:`
+        let italics = ''
+
+        if (style === 'italic') italics = 'ital,'
+        familiesFmt.push(
+          `${query}${italics}wght@${italics ? '1,' : ''}${weight}`
+        )
+      })
+    })
+  })
+
+  return `https://fonts.googleapis.com/css2?${familiesFmt.join('&')}`
+}
+
+export const getFontFamilyHref = async (font: string) => {
+  // const fontArr = fonts.split(',') // remove preceeding ' 's
+  // const fontFamilies = fontArr.map((v: any) => v?.stack || v).join(',')
+
+  const res = await fetch(`https://components.ai/api/v1/typefaces/${font}`)
+  const rawFontData = await res.json()
+  
+  //TODO: this will break for system fonts
+  const styles = Object.keys(rawFontData?.variants)
+  const weights = Object.keys(rawFontData?.variants[styles[0]])
+  const fontData: FontFamily = {
+    name: font,
+    weights,
+    styles
+  }
+  
+  return await toGoogleFontUrl([fontData])
+}
+
+import * as React from 'react'
 export const Fonts = ({fontFamily}: any) => {
-  // const fontStyleSheetHref = getFontFamilyHref(fonts)
+  
+  const [styleSheet, setStyleSheet] = React.useState<string | null>('')
+  React.useEffect(() => {
+    // debounce
+    const getStyleSheet = async () => {
+      const ss = await getFontFamilyHref(fontFamily)
+      setStyleSheet(ss)
+    }
+
+    getStyleSheet()
+  }, [fontFamily])
+
+  
+  console.log(styleSheet, "family")
+  return (
+    <>
+      {styleSheet ? (
+        <link rel="stylesheet" href={styleSheet} />
+      ) : null}
+    </>
+  )
   // const variableFontsStylesheetHref = getVariableFontFamilyHref(
   //   fonts,
   //   textStyles
