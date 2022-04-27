edad0efbaa881d4b263e23170d80359eb83253c1
Dean Blacc <deanblacc@googlemail.com>
2022-04-27 23:07:41 +0100

    should work now

--- packages/gui/src/components/FontFamily/FontFamilyInput.tsx
+++ packages/gui/src/components/FontFamily/FontFamilyInput.tsx
@@ -240,22 +240,22 @@ const enum FontCategory {
 const getAllOptions = async (): Promise<Font[]> => {
   const opts: Font[] = []
   const rawGoogData = await fetch('https://components.ai/api/v1/typefaces/list')
-  // const rawSystemData = await fetch('https://components.ai/api/v1/typefaces/system')
+  const rawSystemData = await fetch('https://components.ai/api/v1/typefaces/system')
 
   // system fonts 
-  // const systemFonts = (await rawSystemData.json()) as any
-  // systemFonts.forEach(({ name, type }: any) => {
-  //   if (
-  //     type === FontCategory.Sans ||
-  //     type === FontCategory.Serif ||
-  //     type === FontCategory.Mono
-  //   ) {
-  //     opts.push({
-  //       fontName: name,
-  //       category: type,
-  //     })
-  //   }
-  // })
+  const systemFonts = (await rawSystemData.json()) as any
+  systemFonts.forEach(({ name, type }: any) => {
+    if (
+      type === FontCategory.Sans ||
+      type === FontCategory.Serif ||
+      type === FontCategory.Mono
+    ) {
+      opts.push({
+        fontName: name,
+        category: type,
+      })
+    }
+  })
 
   const googFontsData = (await rawGoogData.json()) as any
   googFontsData.forEach((font: any) => {
