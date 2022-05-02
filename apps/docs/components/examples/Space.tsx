7cf0ae21eb886ae65dd42d152be589c3fc815800
John Otander <johnotander@gmail.com>
2022-05-02 13:46:27 -0600

    Fix build

--- apps/docs/components/examples/Space.tsx
+++ apps/docs/components/examples/Space.tsx
@@ -9,12 +9,14 @@ export const SpaceExample = () => {
 
   return (
     <div>
+      {/** @ts-ignore */}
       <Editor styles={styles} onChange={setStyles} />
       <div sx={{ border: 'thin solid', borderColor: 'border', mt: [3, 4, 5] }}>
         <styled.p styles={{ ...styles, border: 'thin solid' }}>
           Fun with space!
         </styled.p>
       </div>
+      {/** @ts-ignore */}
       <pre>{codegen.css(styles)}</pre>
       <hr />
       <pre>{JSON.stringify(styles, null, 2)}</pre>
