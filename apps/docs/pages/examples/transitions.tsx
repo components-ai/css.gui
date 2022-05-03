be021d41dc9513e8a371d49c1a49de36a5d3d77f
mrmrs <hi@mrmrs.cc>
2022-05-02 21:47:10 -0700

    Changes order of properties and devault value for transition property

--- apps/docs/pages/examples/transitions.tsx
+++ apps/docs/pages/examples/transitions.tsx
@@ -2,8 +2,6 @@ import { Editor, styled } from '@compai/css-gui'
 import { useState } from 'react'
 
 const initialStyles = {
-  transitionDuration: { value: 250, unit: 'ms' },
-  transitionDelay: { value: 0, unit: 'ms' },
   transitionTimingFunction: {
     type: 'cubic-bezier',
     p1: 0,
@@ -11,9 +9,12 @@ const initialStyles = {
     p3: 1,
     p4: 1,
   },
-  transitionProperty: 'height',
+  transitionDuration: { value: 250, unit: 'ms' },
+  transitionDelay: { value: 0, unit: 'ms' },
+  transitionProperty: 'all',
   width: { value: 200, unit: 'px' },
   height: { value: 200, unit: 'px' },
+  borderRadius: { value: 0, unit: 'px' },
   backgroundColor: '#f00',
 }
 
@@ -21,8 +22,12 @@ export default function Transitions() {
   const [styles, setStyles] = useState<any>(initialStyles)
   return (
     <div>
-      <Editor styles={styles} onChange={setStyles} />
-      <styled.div styles={styles} />
+      <div sx={{ ':first-child > div' : { display: 'grid', gap: '.5em' }}}>
+        <Editor styles={styles} onChange={setStyles} />
+      </div>
+      <section sx={{ mt: 5 }}>
+        <styled.div styles={styles} />
+      </section>
     </div>
   )
 }
