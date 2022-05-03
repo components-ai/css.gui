1a8c94eadc918603c4aaed8adf62ae358221e69f
Dean Blacc <deanblacc@googlemail.com>
2022-05-03 12:39:42 +0100

    change this temp bit for easier management

--- packages/gui/src/components/RenderElement.tsx
+++ packages/gui/src/components/RenderElement.tsx
@@ -14,21 +14,23 @@ export const RenderElement = ({
   ...props
 }: RenderElementProps) => {
   const Component = tagName
+  //Temp
+  const { fontFamily } = styles
   const styleObject = toCSSObject(styles)
   
   
-  // console.log(styles, "stttlses")
   // TODO: change all this back
   return (
     // @ts-ignore
     <>
       {/* @ts-ignore */}
-      <FontTags fontFamily={styles.fontFamily.fontFamily}/>
+      <FontTags fontFamily={fontFamily.fontFamily}/>
       {/* @ts-ignore */}
       <Component {...props} sx={{ 
         ...styleObject,
         // @ts-ignore
-        fontFamily: styles.fontFamily.fontFamily,
+        ...fontFamily,
+        // fontFamily: styles.fontFamily.fontFamily,
         // @ts-ignore
         fontVariationSettings: styles.fontFamily?.fontVariationSettings
       }} />
