65de63ee1be415a069871e93e635d257ee311637
John Otander <johnotander@gmail.com>
2022-05-03 13:51:27 -0600

    Move static dir to preferred public dir

--- apps/docs/pages/_app.tsx
+++ apps/docs/pages/_app.tsx
@@ -6,7 +6,7 @@ import { Sidebar } from '../components/Sidebar'
 import { PageWrap } from '../components/PageWrap'
 import { Layout } from '../components/Layout'
 import { Head } from '../components/Head'
-import '../static/code-styles.css'
+import '../public/code-styles.css'
 
 Router.events.on('routeChangeComplete', (url) => {
   // @ts-ignore
