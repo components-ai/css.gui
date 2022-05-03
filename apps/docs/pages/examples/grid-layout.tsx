1761a46588c5c24a849c31d3461b948cb82f74f4
Harley Turan <harley@hturan.com>
2022-05-04 00:45:14 +0100

    integrate editor, add text

--- apps/docs/pages/examples/grid-layout.tsx
+++ apps/docs/pages/examples/grid-layout.tsx
@@ -6,18 +6,28 @@ const createElementStyles = () => ({
   gridRowEnd: Math.ceil(Math.random() * 6),
   gridColumnStart: Math.ceil(Math.random() * 6),
   gridColumnEnd: Math.ceil(Math.random() * 6),
-  backgroundColor: 'red.' + Math.floor(Math.random() * 10),
   offset: Math.random(),
 })
 
-export default function Shadows() {
-  const [elements, setElements] = useState([createElementStyles()])
+const initialStyles = {
+  transform: [
+    { type: 'perspective', amount: { value: 512, unit: 'px' } },
+    { type: 'rotateX', amount: { value: 15, unit: 'deg' } },
+    { type: 'rotateY', amount: { value: 15, unit: 'deg' } },
+  ],
+}
 
-  const [perspective, setPerspective] = useState(256)
+export default function Shadows() {
+  const [styles, setStyles] = useState<any>(initialStyles)
+
+  const [elements, setElements] = useState([createElementStyles()])
   const [offset, setOffset] = useState(128)
+  const [string, setString] = useState('components.ai')
 
   return (
     <div>
+      <Editor styles={styles} onChange={setStyles} />
+
       <button
         onClick={(e) => setElements([...elements, createElementStyles()])}
       >
@@ -29,12 +39,9 @@ export default function Shadows() {
 
       <div>
         <input
-          type="range"
-          value={perspective}
-          onChange={(e) => setPerspective(parseInt(e.target.value))}
-          min={0}
-          max={512}
-          step={1}
+          type="text"
+          value={string}
+          onChange={(e) => setString(e.target.value)}
         />
         <br />
         <input
@@ -47,30 +54,43 @@ export default function Shadows() {
         />
       </div>
 
-      <pre sx={{ width: 3 }}>
+      <pre sx={{ position: 'absolute' }}>
         {elements.map((el) => JSON.stringify(el) + '\n')}
       </pre>
 
-      <div
-        sx={{
+      <styled.div
+        styles={{
           m: 4,
           display: 'grid',
           justifyContent: 'center',
           transformStyle: 'preserve-3d',
           pointerEvents: 'none',
-          transform: `perspective(${perspective}px) rotateX(24deg) rotateY(36deg)`,
+          ...styles,
         }}
       >
-        {elements.map((el) => (
-          <div
-            sx={{
-              ...el,
-              p: 3,
-              transform: `translateZ(${offset * el.offset}px)`,
-            }}
-          ></div>
-        ))}
-      </div>
+        {elements.map((el, i) => {
+          const deltaY = Math.abs(el.gridRowStart - el.gridRowEnd)
+          const deltaX = Math.abs(el.gridColumnStart - el.gridColumnEnd)
+
+          const area = (deltaX * deltaY) / 12
+
+          return (
+            <div
+              sx={{
+                ...el,
+                p: 3,
+                fontWeight: 900,
+                fontSize: Math.ceil((deltaX + 1) * 18) + 'px',
+                transform: `translateZ(${offset * (el.offset - 0.5)}px)`,
+                border: '1px solid',
+                borderColor: 'red.7',
+              }}
+            >
+              {string[i]}
+            </div>
+          )
+        })}
+      </styled.div>
     </div>
   )
 }
