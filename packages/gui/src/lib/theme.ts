9af74d6c10810adc8c92deb08d2ac618debfd84f
John Otander <johnotander@gmail.com>
2022-05-03 10:43:06 -0600

    Add theme transformers and theme schema docs
    Closes #99

--- packages/gui/src/lib/theme.ts
+++ packages/gui/src/lib/theme.ts
@@ -1,4 +1,55 @@
+import { v4 as uuid } from 'uuid'
+import { Theme, ColorModesScale } from 'theme-ui'
 import { THEME_SCALES } from './constants'
+import { ColorGroup, Theme as CSSGUITheme } from '../types/theme'
 
 export const isThemeable = (property?: string): boolean =>
   !!THEME_SCALES[property || '']
+
+/**
+ * Turn a system-ui color object into an internally compatible shape
+ *
+ * {
+ *   gray: ['#000', '#111', '#999'],
+ *   tomato: 'tomato'
+ * }
+ */
+export const importColors = (colors?: ColorModesScale): ColorGroup[] => {
+  if (!colors) {
+    return []
+  }
+
+  return Object.entries(colors).map(([name, group]) => {
+    if (!Array.isArray(group)) {
+      return {
+        id: uuid(),
+        name,
+        colors: [
+          {
+            id: uuid(),
+            value: group,
+          },
+        ],
+      }
+    }
+
+    return {
+      id: uuid(),
+      name,
+      colors: group.map((value) => {
+        return {
+          id: uuid(),
+          value: value,
+        }
+      }),
+    }
+  })
+}
+
+export const importThemeFromSpec = (theme: Theme): CSSGUITheme => {
+  const colors = importColors(theme.colors)
+
+  return {
+    colors,
+  }
+}
