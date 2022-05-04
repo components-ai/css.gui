c3905d668566ab4fb6dbeb65fe090d91be714363
John Otander <johnotander@gmail.com>
2022-05-04 11:24:58 -0600

    Show recursive by default in font family example

--- apps/docs/components/examples/FontFamily.tsx
+++ apps/docs/components/examples/FontFamily.tsx
@@ -2,13 +2,11 @@ import { useState } from 'react'
 import { FontFamilyInput, styled } from '@compai/css-gui'
 
 export const FontFamilyExample = () => {
-  const [styles, setStyles] = useState({ fontFamily: 'Abel'})
+  const [styles, setStyles] = useState({ fontFamily: 'Recursive' })
 
   return (
     <>
-      <styled.p styles={styles}>
-        Fun with fonts!
-      </styled.p>
+      <styled.p styles={styles}>Fun with fonts!</styled.p>
       <FontFamilyInput value={styles} onChange={setStyles} />
     </>
   )
