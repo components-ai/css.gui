8a27ea1498f9dd04040678f76f049d919f21e0b7
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2022-05-03 20:40:34 -0700

    add rest of the masks

---
+++ packages/gui/src/components/inputs/Mask/field.tsx
@@ -0,0 +1,98 @@
+import { EditorProps } from '../../../types/editor'
+
+import { stringifyMaskList } from './stringify'
+
+import Layers from '../../Layers'
+import { EditorPropsWithLabel, getInputProps } from '../../../lib/util'
+import { ImageSourceEditor } from '../ImageSource/field'
+import { PositionInput } from '../PositionInput'
+import { SelectInput } from '../SelectInput'
+import {
+  geometryBoxKeywords,
+  Mask,
+  compositingOperators,
+  maskingModes,
+} from './types'
+import { RepeatStyleInput, SizeInput } from '../Background/field'
+
+export default function MaskInput(props: EditorPropsWithLabel<Mask[]>) {
+  const newItem = () => {
+    // generate a new text shadow with the units of the previous box shadow
+    return {
+      clip: 'border-box',
+      image: {
+        type: 'gradient',
+        gradient: {
+          type: 'linear',
+          degrees: 0,
+          stops: [],
+        },
+      },
+      origin: 'border-box',
+      position: {
+        x: { value: 0, unit: 'px' },
+        y: { value: 0, unit: 'px' },
+      },
+      repeat: {
+        x: 'no-repeat',
+        y: 'no-repeat',
+      },
+      size: {
+        x: { value: 100, unit: '%' },
+        y: { value: 100, unit: '%' },
+      },
+      composite: 'add',
+      mode: 'alpha',
+    } as any
+  }
+  return (
+    <Layers<Mask>
+      {...props}
+      newItem={newItem}
+      content={MaskLayer}
+      stringify={stringifyMaskList}
+      thumbnail={Thumbnail}
+    />
+  )
+}
+
+export const MaskLayer = (props: EditorProps<Mask>) => {
+  return (
+    <div sx={{ m: 3, display: 'flex', flexDirection: 'column', gap: 3 }}>
+      <ImageSourceEditor {...getInputProps(props, 'image')} />
+      <PositionInput {...getInputProps(props, 'position')} />
+      <SizeInput {...getInputProps(props, 'size')} />
+      <RepeatStyleInput {...getInputProps(props, 'repeat')} />
+      <SelectInput
+        {...getInputProps(props, 'origin')}
+        options={geometryBoxKeywords}
+      />
+      <SelectInput
+        {...getInputProps(props, 'clip')}
+        options={[...geometryBoxKeywords, 'no-clip']}
+      />
+      <SelectInput
+        {...getInputProps(props, 'composite')}
+        options={compositingOperators}
+      />
+      <SelectInput {...getInputProps(props, 'mode')} options={maskingModes} />
+    </div>
+  )
+}
+
+function Thumbnail({ value }: { value: string }) {
+  return (
+    <div
+      sx={{
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        mask: value,
+        backgroundImage:
+          'conic-gradient(#f00, #ff0, #0f0, #0ff, #00f, #f0f, #f00)',
+      }}
+    ></div>
+  )
+}
