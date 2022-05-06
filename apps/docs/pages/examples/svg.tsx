93a829160bb6485858ba30e280bd3401efa8cbfb
mrmrs <hi@mrmrs.cc>
2022-05-05 20:28:33 -0700

    Adds additional properties for stroke to svg example page

--- apps/docs/pages/examples/svg.tsx
+++ apps/docs/pages/examples/svg.tsx
@@ -6,9 +6,39 @@ import { Container } from '../../components/Container'
 const initialStyles = {
   stroke: '#fff',
   fill: 'none',
+  strokeAlignment: {
+    value: 'center',
+    unit: 'keyword',
+  },
+  strokeWidth: {
+    value: 8,
+    unit: 'px',
+  },
+  strokeLinejoin: {
+    value: 'round',
+    unit: 'keyword',
+  },
+  strokeLinecap: {
+    value: 'square',
+    unit: 'keyword',
+  },
+  strokeDashadjust: {
+    value: 'dashed',
+    unit: 'keyword',
+  },
+  strokeDashcorner: {
+    value: 0,
+    unit: 'px',
+  },
+  strokeDashoffset: {
+    value: 0,
+    unit: 'px',
+  },
   strokeDasharray: [
-    { value: 10, unit: 'number' },
-    { value: 5, unit: 'number' },
+    { value: 0, unit: 'number' },
+    { value: 8, unit: 'number' },
+    { value: 0, unit: 'number' },
+    { value: 24, unit: 'number' },
   ],
 }
 export default function SvgExample() {
@@ -24,11 +54,15 @@ export default function SvgExample() {
         <svg width="400" height="400">
           <circle sx={{ ...toCSSObject(styles) }} cx={200} cy={200} r={150} />
         </svg>
+        <section sx={{ '& > div': {
+          display: 'grid', gap: '1em',
+          }}}>
         <Editor
           styles={styles}
           onChange={setStyles}
           theme={defaultTheme}
         ></Editor>
+        </section>
         </div>
     </div>
   )
