afd7eb869c90f579fd75cd84cc58b2a08a7a5735
mrmrs <hi@mrmrs.cc>
2022-05-16 18:10:29 -0700

    Adds missing } at end of file

--- apps/docs/data/initial-styles-border-top-left-radius-preview.ts
+++ apps/docs/data/initial-styles-border-top-left-radius-preview.ts
@@ -2,13 +2,14 @@ export const initialStyles: any = {
   borderTopLeftRadius: {
     type: 'multidimensionalLength',
     values: [
-    {
-      value: 16,
-      unit: 'px',
-    },
-    {
-      value: 16,
-      unit: 'px',
-    }
-  ],
+      {
+        value: 16,
+        unit: 'px',
+      },
+      {
+        value: 16,
+        unit: 'px',
+      }
+    ],
+  }
 }
