09b9fbc2f2b83e55cb14b7cef8d6fd0791092c48
mrmrs <hi@mrmrs.cc>
2022-05-16 18:05:55 -0700

    Adds initial styles for border-top-left-radius

---
+++ apps/docs/data/initial-styles-border-top-left-radius-preview.ts
@@ -0,0 +1,14 @@
+export const initialStyles: any = {
+  borderTopLeftRadius: {
+    type: 'multidimensionalLength',
+    values: [
+    {
+      value: 16,
+      unit: 'px',
+    },
+    {
+      value: 16,
+      unit: 'px',
+    }
+  ],
+}
