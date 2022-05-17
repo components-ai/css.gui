e35b530e747b657483c2f4a5d6d4735f2fc03c24
John Otander <johnotander@gmail.com>
2022-05-17 13:18:30 -0600

    Fix build

--- apps/docs/components/examples/TransitionPreview.tsx
+++ apps/docs/components/examples/TransitionPreview.tsx
@@ -1,27 +1,41 @@
 import { useState } from 'react'
 import Link from 'next/link'
 import { Editor, Fieldset, Inputs, styled, codegen } from '@compai/css-gui'
-import { initialStyles } from '../../data/initial-styles-transition-preview'
+//import { initialStyles } from '../../data/initial-styles-transition-preview'
 import { defaultTheme } from '../../data/default-theme'
+
+const initialStyles = {}
 
 export function TransitionPreview() {
   const [styles, setStyles] = useState<any>(initialStyles)
   return (
     <>
-      <article 
-      id='transition'
-      sx={{ 
-        color: 'muted',
-        width: '100%', 
-        boxShadow: 'inset 0 0 0px 1px currentColor', 
-        borderRadius: '6px',
-        overflow: 'hidden',
-        cursor: 'pointer',
-        }}>
-        <code sx={{ color: 'text', px: 3, py: 2, width: '100%', display: 'block', borderBottom: '1px solid', borderBottomColor: 'muted', }}>
+      <article
+        id="transition"
+        sx={{
+          color: 'muted',
+          width: '100%',
+          boxShadow: 'inset 0 0 0px 1px currentColor',
+          borderRadius: '6px',
+          overflow: 'hidden',
+          cursor: 'pointer',
+        }}
+      >
+        <code
+          sx={{
+            color: 'text',
+            px: 3,
+            py: 2,
+            width: '100%',
+            display: 'block',
+            borderBottom: '1px solid',
+            borderBottomColor: 'muted',
+          }}
+        >
           &lt;Inputs.Transition /&gt;
         </code>
-      <section sx={{
+        <section
+          sx={{
             fontSize: '48px',
             lineHeight: '1.5',
             fontWeight: 700,
@@ -31,33 +45,36 @@ export function TransitionPreview() {
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'center',
-          }}>
-          <styled.a 
-              styles={styles} 
-              style={{ 
-                //borderRadius: '6px',
-                overflow: 'hidden',
-                margin: 0, 
-                lineHeight: 1, 
-                height: '60%', 
-                width: '100%', 
-                display: 'flex', 
-                alignItems: 'center', 
-                justifyContent: 'center', 
-                backgroundImage: 'url(https://source.unsplash.com/random/1920x1080)',
-                backgroundSize: 'cover',
-                backgroundPosition: 'center center',
-                transition: 'all .2s ease-in-out',
-              }}>
-          </styled.a>
+          }}
+        >
+          <styled.a
+            styles={styles}
+            style={{
+              //borderRadius: '6px',
+              overflow: 'hidden',
+              margin: 0,
+              lineHeight: 1,
+              height: '60%',
+              width: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundImage:
+                'url(https://source.unsplash.com/random/1920x1080)',
+              backgroundSize: 'cover',
+              backgroundPosition: 'center center',
+              transition: 'all .2s ease-in-out',
+            }}
+          ></styled.a>
         </section>
         <Editor styles={styles} onChange={setStyles} theme={defaultTheme}>
           <div
             sx={{
               color: 'text',
-              px: 3, pt: 3, 
+              px: 3,
+              pt: 3,
               width: '100%',
-              '& > div': { 
+              '& > div': {
                 display: 'grid',
                 gap: '.5rem',
               },
@@ -66,23 +83,25 @@ export function TransitionPreview() {
             <Inputs.Transition />
           </div>
         </Editor>
-        
-<div sx={{ 
-        px: 3,
-        pb: 4,
-        maxWidth: '100%',
-        color: 'text',
-        overflow: 'auto',
-        }}>
-        <pre
+
+        <div
           sx={{
-            width: '100%',
-            fontSize: 0,
+            px: 3,
+            pb: 4,
+            maxWidth: '100%',
+            color: 'text',
+            overflow: 'auto',
           }}
         >
-          {codegen.css(styles)}
-        </pre>
-      </div>
+          <pre
+            sx={{
+              width: '100%',
+              fontSize: 0,
+            }}
+          >
+            {codegen.css(styles)}
+          </pre>
+        </div>
       </article>
     </>
   )
