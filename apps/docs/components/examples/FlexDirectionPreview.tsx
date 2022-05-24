60842c6af8f773e1428badfeb08a85abb5661cae
John Otander <johnotander@gmail.com>
2022-05-24 07:46:59 -0600

    Fix build error

--- apps/docs/components/examples/FlexDirectionPreview.tsx
+++ apps/docs/components/examples/FlexDirectionPreview.tsx
@@ -9,50 +9,104 @@ export function FlexDirectionPreview() {
 
   return (
     <>
-      <article 
-      id='flex-direction'
-      sx={{ 
-        color: 'muted',
-        width: '100%', 
-        boxShadow: 'inset 0 0 0px 1px currentColor', 
-        borderRadius: '6px',
-        overflow: 'hidden',
-        }}>
-      
-        <code sx={{ color: 'text', px: 3, py: 2, width: '100%', display: 'block', borderBottom: '1px solid', borderBottomColor: 'muted', }}>
+      <article
+        id="flex-direction"
+        sx={{
+          color: 'muted',
+          width: '100%',
+          boxShadow: 'inset 0 0 0px 1px currentColor',
+          borderRadius: '6px',
+          overflow: 'hidden',
+        }}
+      >
+        <code
+          sx={{
+            color: 'text',
+            px: 3,
+            py: 2,
+            width: '100%',
+            display: 'block',
+            borderBottom: '1px solid',
+            borderBottomColor: 'muted',
+          }}
+        >
           &lt;Inputs.FlexDirection /&gt;
         </code>
-        <section sx={{
+        <section
+          sx={{
             fontSize: '10px',
             height: '192px',
             px: 4,
             py: 3,
             borderBottom: '1px solid',
-          }}>
+          }}
+        >
           <styled.article
-          styles={styles}
-            style={{ 
+            styles={styles}
+            style={{
               color: 'text',
-              display: 'flex', 
+              display: 'flex',
               paddingTop: '.5rem',
               paddingBottom: '.5rem',
               alignItems: 'stretch',
-              margin: 0, 
+              margin: 0,
               lineHeight: '1.5',
               height: '100%',
-            }}>
-            <div sx={{ color: 'text', flexBasis: '100px', flexGrow: 1, maxHeight: '100%', minHeight: 48, padding: '8px',  outline: '1px solid', color: 'text', }}>1</div>
-            <div sx={{ flexBasis: '100px', flexGrow: 1, flexShrink: 1, width: '100%', maxHeight: '100%', minHeight: 48, p: 2, outline: '1px solid', color: 'text', }}>2</div>
-            <div sx={{ flexBasis: '100px', flexGrow: 1, flexShrink: 1, width: '100%', maxHeight: '100%', minHeight: 48, p: 2, outline: '1px solid', color: 'text', }}>3</div>
+            }}
+          >
+            <div
+              sx={{
+                color: 'text',
+                flexBasis: '100px',
+                flexGrow: 1,
+                maxHeight: '100%',
+                minHeight: 48,
+                padding: '8px',
+                outline: '1px solid',
+              }}
+            >
+              1
+            </div>
+            <div
+              sx={{
+                flexBasis: '100px',
+                flexGrow: 1,
+                flexShrink: 1,
+                width: '100%',
+                maxHeight: '100%',
+                minHeight: 48,
+                p: 2,
+                outline: '1px solid',
+                color: 'text',
+              }}
+            >
+              2
+            </div>
+            <div
+              sx={{
+                flexBasis: '100px',
+                flexGrow: 1,
+                flexShrink: 1,
+                width: '100%',
+                maxHeight: '100%',
+                minHeight: 48,
+                p: 2,
+                outline: '1px solid',
+                color: 'text',
+              }}
+            >
+              3
+            </div>
           </styled.article>
         </section>
         <Editor styles={styles} onChange={setStyles} theme={defaultTheme}>
           <div
             sx={{
               color: 'text',
-              px: 3, pt: 3,
+              px: 3,
+              pt: 3,
               width: '100%',
-              '& > div': { 
+              '& > div': {
                 display: 'grid',
                 gap: '.5rem',
               },
@@ -61,22 +115,24 @@ export function FlexDirectionPreview() {
             <Inputs.FlexDirection />
           </div>
         </Editor>
-<div sx={{ 
-        px: 3,
-        pb: 3,
-        maxWidth: '100%',
-        color: 'text',
-        overflow: 'auto',
-        }}>
-        <pre
+        <div
           sx={{
-            width: '100%',
-            fontSize: 0,
+            px: 3,
+            pb: 3,
+            maxWidth: '100%',
+            color: 'text',
+            overflow: 'auto',
           }}
         >
-          {codegen.css(styles)}
-        </pre>
-      </div>
+          <pre
+            sx={{
+              width: '100%',
+              fontSize: 0,
+            }}
+          >
+            {codegen.css(styles)}
+          </pre>
+        </div>
       </article>
     </>
   )
