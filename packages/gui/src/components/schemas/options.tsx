ced0cb3bbf348d9a40f4f77d0e31b4a78f69cd79
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2022-05-29 00:51:58 -0700

    clean up typing

--- packages/gui/src/components/schemas/options.tsx
+++ packages/gui/src/components/schemas/options.tsx
@@ -3,7 +3,7 @@ import { SelectInput } from '../inputs/SelectInput'
 import { Label } from '../primitives'
 import { DataTypeSchema } from './types'
 
-interface CreateOptions<T> {
+interface CreateOptions<T extends Record<string, any>> {
   variants: { [V in keyof T]: DataTypeSchema<T[V]> }
   order?: (keyof T)[]
   stringify?(variant: keyof T, value: string): string
@@ -15,7 +15,7 @@ interface CreateOptions<T> {
  */
 export function optionsSchema<T extends Record<string, any>>({
   variants,
-  order = Object.keys(variants) as any,
+  order = Object.keys(variants),
   stringify = (variant, value) => value,
 }: CreateOptions<T>): DataTypeSchema<Unionize<T>> {
   return {
@@ -27,7 +27,7 @@ export function optionsSchema<T extends Record<string, any>>({
           <Label>{props.label}</Label>
           <SelectInput
             {...getInputProps(props, 'type')}
-            options={order}
+            options={order as string[]}
             onChange={(newType) => {
               // if the type changes, reset the value to the default value of that type
               props.onChange({
@@ -53,7 +53,4 @@ export function optionsSchema<T extends Record<string, any>>({
 
 // Utility types to help typecheck these options correctly,
 // based on: https://stackoverflow.com/questions/62591230/typescript-convert-a-tagged-union-into-an-union-type
-type SingleVariant<T, K extends keyof T> = K extends any
-  ? { type: K } & T[K]
-  : never
-type Unionize<T> = { [K in keyof T]: SingleVariant<T, K> }[keyof T]
+type Unionize<T> = { [K in keyof T]: { type: K } & T[K] }[keyof T]
