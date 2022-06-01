73f3ff4f06dc5767cd1ef9f562a6fc11a625fd13
John Otander <johnotander@gmail.com>
2022-05-31 18:35:45 -0600

    Add label to make TS happy

--- apps/docs/components/examples/Dimension.tsx
+++ apps/docs/components/examples/Dimension.tsx
@@ -14,6 +14,7 @@ export const DimensionExample = () => {
     <div>
       <div sx={{ width: 256, ml: 0 }}>
         <DimensionInput
+          label="Font Size"
           value={value}
           onChange={setValue}
           units={['px', 'em', 'rem']}
