50fe68bd08d8c79031debb18a7e5f5465b287397
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2022-06-01 17:43:26 -0700

    generalize select styling

--- packages/gui/src/components/primitives/KeywordSelect.tsx
+++ packages/gui/src/components/primitives/KeywordSelect.tsx
@@ -1,5 +1,4 @@
-import * as Select from '@radix-ui/react-select'
-import { ChevronDown } from 'react-feather'
+import * as Select from '../ui/Select'
 import { GLOBAL_KEYWORDS } from '../../data/global-keywords'
 import { EditorProps } from '../../types/editor'
 
@@ -18,63 +17,19 @@ export const KeywordSelect = <T extends string>({
   const allOptions = topLevel ? [...options, ...GLOBAL_KEYWORDS] : options
   return (
     <Select.Root value={value} onValueChange={onChange}>
-      <Select.Trigger
-        sx={{
-          minHeight: '1.6em',
-          background: 'none',
-          border: 'none',
-          color: 'text',
-          width: 'max-content',
-          ':hover': {
-            background: 'backgroundOffset',
-          },
-        }}
-      >
+      <Select.Trigger>
         <Select.Value />
-        {!hideIcon && (
-          <Select.Icon>
-            <ChevronDown size={14} sx={{ transform: 'translateY(3px)' }} />
-          </Select.Icon>
-        )}
+        {!hideIcon && <Select.Icon />}
       </Select.Trigger>
-      <Select.Content
-        sx={{
-          backgroundColor: 'background',
-          text: 'text',
-          py: 1,
-          border: '1px solid',
-          borderColor: 'border',
-          borderRadius: '0.5rem',
-          fontSize: 1,
-        }}
-      >
-        <Select.Viewport>
-          {allOptions.map((option) => {
-            return (
-              <Select.Item
-                value={option as any}
-                sx={{
-                  pr: 3,
-                  pl: 4,
-                  cursor: 'pointer',
-                  ':hover': {
-                    backgroundColor: 'primary',
-                  },
-                }}
-              >
-                <Select.ItemIndicator
-                  sx={{
-                    position: 'absolute',
-                    left: 3,
-                  }}
-                >
-                  ✓
-                </Select.ItemIndicator>
-                <Select.ItemText>{option}</Select.ItemText>
-              </Select.Item>
-            )
-          })}
-        </Select.Viewport>
+      <Select.Content>
+        {allOptions.map((option) => {
+          return (
+            <Select.Item value={option as any}>
+              <Select.ItemIndicator />
+              <Select.ItemText>{option}</Select.ItemText>
+            </Select.Item>
+          )
+        })}
       </Select.Content>
     </Select.Root>
   )
