df48638fe603f2d9de417f53d8a536e766cce203
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2022-06-03 19:06:22 -0700

    standardize more inputs

--- packages/gui/src/components/inputs/TextInput.tsx
+++ packages/gui/src/components/inputs/TextInput.tsx
@@ -1,37 +1,23 @@
-import { kebabCase } from 'lodash-es'
-import { useId } from 'react'
-import { Label } from '../primitives'
-import { DeletePropButton } from './Dimension/Input'
+import { EditorPropsWithLabel } from '../../types/editor'
+import { InputHeader } from '../ui/InputHeader'
 
-interface Props<T extends string> {
-  label: string
-  onChange: (newValue: T) => void
-  onRemove?: () => void
-  value: T
-}
+type Props = EditorPropsWithLabel<string>
+
 export function TextInput<T extends string>({
-  label,
   value,
   onChange,
-  onRemove,
-}: Props<T>) {
-  const id = `${useId()}-${kebabCase(label)}`
-
+  ...props
+}: Props) {
   return (
-    <div>
-      <Label htmlFor={id}>
-        <span>{label}</span>
-        <div sx={{ display: 'flex', flexDirection: 'row' }}>
-          <input
-            type="text"
-            id={id}
-            value={value}
-            onChange={(e) => onChange(e.target.value as T)}
-            sx={{ width: '100%', minHeight: '1.6em', mr: 1 }}
-          />
-          {onRemove && <DeletePropButton onRemove={onRemove} />}
-        </div>
-      </Label>
-    </div>
+    <InputHeader {...props}>
+      <div sx={{ display: 'flex', flexDirection: 'row' }}>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => onChange(e.target.value as T)}
+          sx={{ width: '100%', minHeight: '1.6em', mr: 1 }}
+        />
+      </div>
+    </InputHeader>
   )
 }
