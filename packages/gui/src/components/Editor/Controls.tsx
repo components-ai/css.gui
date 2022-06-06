812d36f4aa7131339667aa60d0950fd0c66ae1ab
John Otander <johnotander@gmail.com>
2022-06-06 16:08:42 -0600

    Fix build

--- packages/gui/src/components/Editor/Controls.tsx
+++ packages/gui/src/components/Editor/Controls.tsx
@@ -22,7 +22,12 @@ import { UnitSteps } from '../../lib'
 import { pascalCase } from '../../lib/util'
 import { UnitRanges } from '../../data/ranges'
 import { AddPropertyControl } from '../AddProperty'
-import { isFieldsetGroup, partitionProperties, sortProperties } from './util'
+import {
+  getDefaultValue,
+  isFieldsetGroup,
+  partitionProperties,
+  sortProperties,
+} from './util'
 import { stylesToEditorSchema } from '../../lib/transformers/styles-to-editor-schema'
 import { removeInternalCSSClassSyntax } from '../../lib/classes'
 import { AddFieldsetControl } from '../AddFieldset'
@@ -316,10 +321,4 @@ function getDefaultsFromChildren(children: ReactNode): Record<string, any> {
     }
   })
   return defaults
-}
-
-function getDefaultValue(property: string) {
-  const propertyDefinition = properties[property] ?? {}
-  // If a default value is defined, return it
-  return propertyDefinition.defaultValue
 }
