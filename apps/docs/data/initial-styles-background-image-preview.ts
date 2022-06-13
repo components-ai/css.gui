4dd987ca71a56cd7354720b86e3beffff812e716
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2022-06-13 16:41:09 -0700

    fix background and bg-image previews

--- apps/docs/data/initial-styles-background-image-preview.ts
+++ apps/docs/data/initial-styles-background-image-preview.ts
@@ -2,7 +2,7 @@ export const initialStyles: any = {
   backgroundImage: [
     {
       type: 'url',
-      value: ['https://source.unsplash.com/random'],
+      value: 'https://source.unsplash.com/random',
     },
   ],
 }
