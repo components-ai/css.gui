c2964f8493f61da6e80239c29d8e84f36a98dc1c
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2022-06-17 01:06:31 -0700

    position offsets

--- packages/gui/src/components/schemas/position.tsx
+++ packages/gui/src/components/schemas/position.tsx
@@ -2,21 +2,35 @@ import { joinSchemas } from './joinSchemas'
 import { objectSchema } from './object'
 import { keyword, lengthPercentage } from './primitives'
 
+const positionX = joinSchemas([
+  keyword(['left', 'center', 'right']),
+  lengthPercentage({
+    defaultValue: { value: 50, unit: '%' },
+  }),
+  objectSchema({
+    type: 'offset',
+    fields: {
+      direction: keyword(['left', 'right']),
+      offset: lengthPercentage(),
+    },
+  }),
+])
+
+const positionY = joinSchemas([
+  keyword(['top', 'center', 'bottom']),
+  lengthPercentage({
+    defaultValue: { value: 50, unit: '%' },
+  }),
+  objectSchema({
+    type: 'offset',
+    fields: {
+      direction: keyword(['top', 'bottom']),
+      offset: lengthPercentage(),
+    },
+  }),
+])
+
 // TODO function so we can set default values
-// TODO offset values
 export const position = objectSchema({
-  fields: {
-    x: joinSchemas([
-      keyword(['left', 'center', 'right']),
-      lengthPercentage({
-        defaultValue: { value: 50, unit: '%' },
-      }),
-    ]),
-    y: joinSchemas([
-      keyword(['top', 'center', 'bottom']),
-      lengthPercentage({
-        defaultValue: { value: 50, unit: '%' },
-      }),
-    ]),
-  },
+  fields: { x: positionX, y: positionY },
 })
