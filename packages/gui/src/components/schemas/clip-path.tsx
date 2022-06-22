fb35bcc91855e87f07f36fa6b5160f600e159402
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2022-06-22 14:29:54 -0700

    shape keywords

--- packages/gui/src/components/schemas/clip-path.tsx
+++ packages/gui/src/components/schemas/clip-path.tsx
@@ -1,18 +1,17 @@
 import { SHAPE_BOX_KEYWORDS } from '../../types/css'
 import { basicShape } from './basic-shape'
+import { joinSchemas } from './joinSchemas'
 import { objectSchema } from './object'
-import { optionsSchema } from './options'
 import { keyword } from './primitives'
 import { url } from './url'
 
-export const clipPath = optionsSchema({
-  variants: {
-    url,
-    shape: objectSchema({
-      fields: {
-        shape: basicShape,
-        box: keyword(SHAPE_BOX_KEYWORDS),
-      },
-    }),
-  },
-})
+export const clipPath = joinSchemas([
+  url,
+  objectSchema({
+    type: '<shape>',
+    fields: {
+      shape: basicShape,
+      box: keyword(SHAPE_BOX_KEYWORDS),
+    },
+  }),
+])
--- packages/gui/src/components/schemas/shape-outside.tsx
+++ packages/gui/src/components/schemas/shape-outside.tsx
@@ -1,21 +1,20 @@
 import { SHAPE_BOX_KEYWORDS } from '../../types/css'
 import { basicShape } from './basic-shape'
 import { image } from './image'
+import { joinSchemas } from './joinSchemas'
 import { objectSchema } from './object'
-import { optionsSchema } from './options'
 import { keyword, lengthPercentage, numberPercentage } from './primitives'
 
-export const shapeOutside = optionsSchema({
-  variants: {
-    image,
-    shape: objectSchema({
-      fields: {
-        shape: basicShape,
-        box: keyword(SHAPE_BOX_KEYWORDS),
-      },
-    }),
-  },
-})
+export const shapeOutside = joinSchemas([
+  image,
+  objectSchema({
+    type: '<shape>',
+    fields: {
+      shape: basicShape,
+      box: keyword(SHAPE_BOX_KEYWORDS),
+    },
+  }),
+])
 
 export const shapeImageThreshold = numberPercentage()
 export const shapeMargin = lengthPercentage()
