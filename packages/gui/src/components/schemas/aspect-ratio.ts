5257ef9fdc5ba031003dec34fbf9ecc1ac5d0fd0
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2022-06-23 22:14:13 -0700

    auto keyword for ratio

--- packages/gui/src/components/schemas/aspect-ratio.ts
+++ packages/gui/src/components/schemas/aspect-ratio.ts
@@ -1,3 +1,5 @@
+import { joinSchemas } from './joinSchemas'
+import { keyword } from './primitives'
 import { ratio } from './ratio'
 
-export const aspectRatio = ratio()
+export const aspectRatio = joinSchemas([ratio(), keyword(['auto'])])
