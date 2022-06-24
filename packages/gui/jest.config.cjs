2b1c5d422d12831593786eb3c5793d50d4718589
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2022-06-23 20:48:10 -0700

    get tests working

---
+++ packages/gui/jest.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest/presets/default-esm', // or other ESM presets
+  // preset: 'ts-jest',
+  testEnvironment: 'node',
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  // extensionsToTreatAsEsm: ['.ts', '.tsx'],
+}
---
+++ packages/gui/src/lib/parse.test.ts
@@ -0,0 +1,6 @@
+import { tokenize } from './parse'
+describe('tokenize', () => {
+  it('works', () => {
+    expect(false).toEqual(true)
+  })
+})
