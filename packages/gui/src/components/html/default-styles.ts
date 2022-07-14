1a8257eadd2f1dd1324c72c853432663be6b4ae3
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2022-07-13 17:48:40 -0700

    remove enums

--- packages/gui/src/components/html/default-styles.ts
+++ packages/gui/src/components/html/default-styles.ts
@@ -1,19 +1,17 @@
-import { HTMLTag } from './types'
-
 export const DEFAULT_STYLES: Record<string, any> = {
-  [HTMLTag.Button]: {},
-  [HTMLTag.A]: {},
-  [HTMLTag.Input]: {},
-  [HTMLTag.H1]: { textAlign: 'left' },
-  [HTMLTag.H2]: { textAlign: 'left' },
-  [HTMLTag.H3]: { textAlign: 'left' },
-  [HTMLTag.H4]: { textAlign: 'left' },
-  [HTMLTag.H5]: { textAlign: 'left' },
-  [HTMLTag.H6]: { textAlign: 'left' },
+  button: {},
+  a: {},
+  input: {},
+  h1: { textAlign: 'left' },
+  h2: { textAlign: 'left' },
+  h3: { textAlign: 'left' },
+  h4: { textAlign: 'left' },
+  h5: { textAlign: 'left' },
+  h6: { textAlign: 'left' },
 }
 
 export const DEFAULT_ATTRIBUTES: Record<string, any> = {
-  [HTMLTag.Svg]: {
+  svg: {
     version: '1.1',
     xmlns: 'http://www.w3.org/2000/svg',
   },
